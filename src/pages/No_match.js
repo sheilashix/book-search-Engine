6139e9fe2ca51cd479b76e244f7862a6b45adb31
sheilashix <70675711+sheilashix@users.noreply.github.com>
2021-07-05 19:22:15 +0300

    Add files via upload

---
+++ src/pages/No_match.js
@@ -0,0 +1,12 @@
+// new -- add if user hits no existing page
+import React from 'react';
+
+const NoMatch = () => {
+  return (
+    <div>
+      Sorry, we couldn't find that page.
+    </div>
+  );
+};
+
+export default NoMatch;
